d7a7d3e20d2b69b5e17fa251464a6a695604970a
usnimoj <jominsu0103@naver.com>
2024-04-06 14:20:35 +0900

    Fix : response message & response status code

--- src/controllers/folderController.js
+++ src/controllers/folderController.js
@@ -3,7 +3,10 @@ const folderService = require("../services/folderService");
 const makeFolder = async (req, res) => {
   try {
     const makefolderResult = await folderService.makeFolder(req);
-    res.status(201).json(makefolderResult);
+    res.status(201).json({
+      message: "New folder creation successful",
+      data: makefolderResult,
+    });
   } catch (error) {
     console.error(error);
     res.status(error.statusCode || 500).json({ message: error.message });
@@ -13,7 +16,12 @@ const makeFolder = async (req, res) => {
 const readFolder = async (req, res) => {
   try {
     const readFolderResult = await folderService.readFolder(req);
-    res.status(200).json({ message: "read success", data: readFolderResult });
+    res
+      .status(200)
+      .json({
+        message: "Successful retrieval of folder list",
+        data: readFolderResult,
+      });
   } catch (error) {
     console.error(error);
     res.status(error.statusCode || 500).json({ message: error.message });
@@ -23,7 +31,12 @@ const readFolder = async (req, res) => {
 const updateFolder = async (req, res) => {
   try {
     const updateFolderResult = await folderService.updateFolder(req);
-    res.status(200).json(updateFolderResult);
+    res
+      .status(201)
+      .json({
+        message: "Folder name modification successful",
+        data: updateFolderResult,
+      });
   } catch (error) {
     console.error(error);
     res.status(error.statusCode || 500).json({ message: error.message });
@@ -33,7 +46,12 @@ const updateFolder = async (req, res) => {
 const deleteFolder = async (req, res) => {
   try {
     const deleteFolderResult = await folderService.deleteFolder(req);
-    res.status(200).json(deleteFolderResult);
+    res
+      .status(200)
+      .json({
+        message: "Folder deletion successful",
+        data: deleteFolderResult,
+      });
   } catch (error) {
     console.error(error);
     res.status(error.statusCode || 500).json({ message: error.message });
